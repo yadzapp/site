eb744d220b92e9a14ed6c4f716a4a2b94a71c14e
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2023-05-20 19:12:32 -0300

    NOT WORKING

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      title: 'YADZ · Yet Another DayZ Launcher',
+      meta: [
+        {
+          charset: 'utf-8'
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, height=device-height, user-scalable=no, initial-scale=1'
+        }
+      ],
+      link: [
+        { rel: 'icon', href: '/favicon.svg' }
+      ],
+      noscript: [
+        { children: 'Javascript is required' }
+      ],
+      bodyAttrs: {
+        class: 'bg-zinc-900 font-sans pattern select-none'
+      }
+    }
+  }, 
+  css: [
+    '~/assets/main.scss'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    Hello. It's working! INSIDE PAGE
+  </div>
+</template>
