88202a37ea30ea413fb9ca71bf8f0729f5640181
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2023-05-21 00:34:03 -0300

    more updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <!-- <img src="@/assets/yadz-logo.svg"> -->
-  <!-- <h1>Yet Another DayZ Launcher</h1> -->
+
+  <!-- Grid 12 cols -->
   <div class="grid grid-cols-12 gap-6 px-6 w-screen h-screen">
-    <div class="flex justify-center items-center col-start-5 col-end-9">
+
+    <!-- Item 4 cols -->
+    <div class="flex justify-center items-center flex-col col-start-5 col-end-9">
+
+      <!-- Logo -->
       <img src="@/assets/yadz-logo.svg">
+      <!-- <h1 class="text-2xl font-bold tracking-tight mt-6">Yet Another DayZ Launcher</h1> -->
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    alert('hey')
+  }
+}
+</script>
