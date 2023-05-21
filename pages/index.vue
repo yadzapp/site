eb9034e3f88f9988693b89a3dc05e0c05502dc2d
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2023-05-21 01:20:18 -0300

    more styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,38 @@
 <template>
 
-  <!-- Grid 12 cols -->
-  <div class="grid grid-cols-12 gap-6 px-6 w-screen h-screen">
+  <div class="flex flex-col justify-between h-screen pb-12">
 
-    <!-- Item 4 cols -->
-    <div class="flex justify-center items-center flex-col col-start-5 col-end-9">
-
-      <!-- Logo -->
-      <img src="@/assets/yadz-logo.svg">
-      <!-- <h1 class="text-2xl font-bold tracking-tight mt-6">Yet Another DayZ Launcher</h1> -->
+    <!-- Logo -->
+    <div class="w-full p-2">
+      <img src="@/assets/yadz-logo.svg" class="w-full">
     </div>
+
+    <!-- Grid 12 cols -->
+    <div class="flex justify-between px-12">
+
+      <p class="">Yet Another DayZ Launcher</p>
+
+      <ul class="flex flex-row space-x-6">
+        <li><a href="" title="Discord" class="underline">Discord</a></li>
+        <li><a href="" title="Twitter" class="underline">Twitter</a></li>
+      </ul>
+
+      <!-- Item 4 cols -->
+      <!-- <div class="flex justify-center items-center flex-col col-start-5 col-end-9"> -->
+
+        <!-- Logo -->
+        <!-- <img src="@/assets/yadz-logo.svg"> -->
+        <!-- <h1 class="text-2xl font-bold tracking-tight mt-6">Yet Another DayZ Launcher</h1> -->
+      <!-- </div> -->
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   mounted() {
-    alert('hey')
+    // alert('hey')
   }
 }
 </script>
