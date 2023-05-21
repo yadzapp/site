a399f2cc24877300e25cc6a1a6e856c1b2495df3
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2023-05-21 01:38:57 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="flex flex-col justify-between h-screen pb-12">
+  <div class="flex flex-col justify-between h-screen pb-8">
 
     <!-- Logo -->
     <div class="w-full p-2">
@@ -8,13 +8,13 @@
     </div>
 
     <!-- Grid 12 cols -->
-    <div class="flex justify-between px-12">
+    <div class="flex justify-between px-8">
 
       <p class="">Yet Another DayZ Launcher</p>
 
-      <ul class="flex flex-row space-x-6">
-        <li><a href="" title="Discord" class="underline">Discord</a></li>
-        <li><a href="" title="Twitter" class="underline">Twitter</a></li>
+      <ul class="flex flex-row space-x-8">
+        <li><a href="https://discord.gg/cGYmqqYT" title="Discord" class="hover:underline">Discord</a></li>
+        <li><a href="https://twitter.com/yadzapp" title="Twitter" class="hover:underline">Twitter</a></li>
       </ul>
 
       <!-- Item 4 cols -->
