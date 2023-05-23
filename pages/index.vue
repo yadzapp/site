8b1641ba3f83e3824a35f7a685600cf42eee7863
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2023-05-22 21:40:30 -0300

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,16 @@
 
     <!-- <div id="bg-image" class="w-full h-4/5"></div> -->
 
-    <div class="bg-image relative w-full h-4/5">
-      <img src="~/assets/bg/bg-image-01.jpg" class="bg-image-item absolute w-full h-full object-cover">
-      <img src="~/assets/bg/bg-image-02.jpg" class="bg-image-item absolute w-full h-full object-cover">
-      <img src="~/assets/bg/bg-image-03.jpg" class="bg-image-item absolute w-full h-full object-cover">
-      <img src="~/assets/bg/bg-image-04.jpg" class="bg-image-item absolute w-full h-full object-cover">
-      <img src="~/assets/bg/bg-image-05.jpg" class="bg-image-item absolute w-full h-full object-cover">
-      <img src="~/assets/bg/bg-image-06.jpg" class="bg-image-item absolute w-full h-full object-cover">
-      <img src="~/assets/bg/bg-image-07.jpg" class="bg-image-item absolute w-full h-full object-cover">
-      <img src="~/assets/bg/bg-image-08.jpg" class="bg-image-item absolute w-full h-full object-cover">
-    </div>
+    <ul class="bg-image relative w-full h-4/5">
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-01.jpg" class="absolute w-full h-full object-cover"></li>
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-02.jpg" class="absolute w-full h-full object-cover"></li>
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-03.jpg" class="absolute w-full h-full object-cover"></li>
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-04.jpg" class="absolute w-full h-full object-cover"></li>
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-05.jpg" class="absolute w-full h-full object-cover"></li>
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-06.jpg" class="absolute w-full h-full object-cover"></li>
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-07.jpg" class="absolute w-full h-full object-cover"></li>
+      <li class="bg-image-item"><img src="~/assets/bg/bg-image-08.jpg" class="absolute w-full h-full object-cover"></li>
+    </ul>
 
     <!-- Grid 12 cols -->
     <div class="grow flex items-center justify-between px-12">
@@ -42,9 +42,17 @@ export default {
   mounted() {
 
     const ImagesContainer = document.querySelector('.bg-image')
-    const ImagesItems = document.querySelectorAll('.bg-image-item')
+    // const ImagesItems = document.querySelectorAll('.bg-image-item')
 
+    // Randomize images
+    // https://stackoverflow.com/a/11972692/1615587
+    for (var i = ImagesContainer.children.length; i >= 0; i--) {
+      ImagesContainer.appendChild(ImagesContainer.children[Math.random() * i | 0]);
+    }
+
+    // Setting a timeout in JS, but it could be a CSS animation
     setTimeout(function () {
+
       setInterval(function(){
         const last = document.querySelector('.bg-image-item:last-child')
         last.classList.add('off')
@@ -54,7 +62,9 @@ export default {
           last.classList.remove('off')
           ImagesContainer.prepend(last)
         }, 2000)
-      }, 8000)
+      }, 10000)
+
+      ImagesContainer.classList.add('on')
     }, 200)
 
   }
