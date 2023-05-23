186ddc369e9fb756da1b16e44504d7f4e2d9cf32
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2023-05-22 21:15:14 -0300

    slideshow style

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,18 @@
 
   <div class="flex flex-col justify-between h-screen">
 
-    <div id="bg-image" class="w-full h-4/5"></div>
+    <!-- <div id="bg-image" class="w-full h-4/5"></div> -->
+
+    <div class="bg-image relative w-full h-4/5">
+      <img src="~/assets/bg/bg-image-01.jpg" class="bg-image-item absolute w-full h-full object-cover">
+      <img src="~/assets/bg/bg-image-02.jpg" class="bg-image-item absolute w-full h-full object-cover">
+      <img src="~/assets/bg/bg-image-03.jpg" class="bg-image-item absolute w-full h-full object-cover">
+      <img src="~/assets/bg/bg-image-04.jpg" class="bg-image-item absolute w-full h-full object-cover">
+      <img src="~/assets/bg/bg-image-05.jpg" class="bg-image-item absolute w-full h-full object-cover">
+      <img src="~/assets/bg/bg-image-06.jpg" class="bg-image-item absolute w-full h-full object-cover">
+      <img src="~/assets/bg/bg-image-07.jpg" class="bg-image-item absolute w-full h-full object-cover">
+      <img src="~/assets/bg/bg-image-08.jpg" class="bg-image-item absolute w-full h-full object-cover">
+    </div>
 
     <!-- Grid 12 cols -->
     <div class="grow flex items-center justify-between px-12">
@@ -13,8 +24,8 @@
       </div>
 
       <ul class="flex flex-row space-x-6">
-        <li class="opacity-50">For updates</li>
-        <li><a href="https://twitter.com/yadzapp" title="Twitter" class="hover:underline">Twitter</a></li>
+        <!-- <li class="opacity-50">Join the conversation</li> -->
+        <li><a href="https://twitter.com/yadzapp" title="Twitter" class="">Twitter</a></li>
       </ul>
     </div>
 
@@ -29,7 +40,23 @@ export default {
     return { yadzLogo };
   },
   mounted() {
-    // alert('hey')
+
+    const ImagesContainer = document.querySelector('.bg-image')
+    const ImagesItems = document.querySelectorAll('.bg-image-item')
+
+    setTimeout(function () {
+      setInterval(function(){
+        const last = document.querySelector('.bg-image-item:last-child')
+        last.classList.add('off')
+
+        setTimeout(function(){
+          last.remove()
+          last.classList.remove('off')
+          ImagesContainer.prepend(last)
+        }, 2000)
+      }, 8000)
+    }, 200)
+
   }
 }
 </script>
